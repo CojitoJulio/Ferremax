efce22fd69f4a8d7c7ba2c90c93b7e82bb2a953d
CojoJulio <dani.gutierrezg@duocuc.cl>
2024-05-13 00:04:24 -0400

    Separacion Fetch
    Se hicieron la mayoria de las separaciones de las peticiones a base de datos en un archivo aparte.

---
+++ FrontEnd/src/Servicios/ServiciosAPI.js
@@ -0,0 +1,17 @@
+export const fetchSucu = async () => {
+    const response = await fetch('http://localhost:3000/api/sucursales');
+    const sucurbd = await response.json()
+    return sucurbd
+}
+
+export const fetchCate = async () => {
+    const response = await fetch('http://localhost:3000/api/categorias');
+    const catedb = await response.json()
+    return catedb;
+}
+
+export const fetchProd = async () => {
+    const response = await fetch('http://localhost:3000/api');
+    const productos = await response.json()
+    return productos;
+}
