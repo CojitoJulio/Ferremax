cc4d7f8cac1fa695778c051c98ed834579f9d80b
CojoJulio <dani.gutierrezg@duocuc.cl>
2024-06-03 01:59:24 -0400

    Update productos.sql

--- productos.sql
+++ productos.sql
@@ -1,5 +1,47 @@
+insert into sucursales(nombre, direccion)
+values ('Providencia', 'Manuel Montt 3824'),
+       ('Puente Alto', 'Casas Viejas 1893'),
+       ('Maipu', '3 Poninente 2913'),
+       ('Las Condes', 'Avenida Vitacura 0924');
+
+-- Categorias
+
+insert into categorias(categoria)
+values ('Tornillos y Anclajes'),
+	   ('Herramientas Manuales'),
+       ('Materiales Basicos'),
+       ('Equipos de Seguridad'),
+       ('Fijaciones y Adhesivos'),
+       ('Equipos de Medicion');
+       
+insert into subcats(categoria_id, subcat, imagen)
+values (1, 'Tornilleria', 'https://superfil.cl/wp-content/uploads/2020/08/Tornillo_Mesa-de-trabajo-1.png'),
+       (2, 'Martillos', 'https://baumart.cl/wp-content/uploads/2024/02/martillo-inoxcrom.webp'),
+       (2, 'Herramientas', 'https://cdnx.jumpseller.com/maqstore/image/35575664/resize/540/540?1684860702'),
+       (2, 'Taladros', 'https://www.triplee.cl/8494-medium_default/taladro-rotomartillo-dewalt-dcd778.jpg'),
+       (2, 'Destornilladores', 'https://www.ferreteriasandina.cl/media/catalog/product/cache/1/image/1000x/82fa5986d0d3a884b873ffa21a2247bd/6/0/60-002.jpg'),
+       (2, 'Llaves', 'https://co.stanleytools.global/LAG/PRODUCT/IMAGES/HIRES/STMT73648-LA/STMT73648-LA_1.jpg?resize=530x530'),
+       (2, 'Cierras', 'https://ferreteriayconstruccion.com.co/img/productos/1574868566.png'),
+       (2, 'Lijadoras', 'https://cdnx.jumpseller.com/maqstore/image/32671436/BO4556.png?1677683617'),
+       (2, 'Materiales de Contruccion', 'https://aguila.cl/media/catalog/product/cache/1/image/1000x/9df78eab33525d08d6e5fb8d27136e95/p/l/pla3111.jpg'),
+       (3, 'Soldadura', 'https://construmartcl.vtexassets.com/arquivos/ids/195020/18901_1.jpg?v=638182803822270000'),
+       (3, 'Cemento', 'https://ebema021123.b-cdn.net/wp-content/uploads/2022/10/502740.jpg'),
+       (3, 'Arena', 'https://photos-us.bazaarvoice.com/photo/2/cGhvdG86ZmFsYWJlbGxh/067ebc35-c07a-5ff1-b6c8-ab219b47dea7'),
+       (3, 'Ladrillos', 'https://constructor.lacuarta.com/wp-content/uploads/2022/07/Ladrillo-fiscal.jpg'),
+       (3, 'Pinturas', 'https://construmartcl.vtexassets.com/arquivos/ids/204250/232365_1.jpg?v=638270745181900000'),
+       (3, 'Barnices', 'https://www.weitzler.cl/bitobee/wp-content/uploads/2022/11/1000500349-1.jpg'),
+       (5, 'Ceramicos', 'https://easycl.vtexassets.com/arquivos/ids/773841/1694446061483_162090-03.jpg?v=638300428928500000'),
+       (3, 'Cables', 'https://easycl.vtexassets.com/arquivos/ids/418415/1628713503205_1258918-01.jpg?v=637643103119770000'),
+       (4, 'Lentes de Seguridad', 'https://urrea.com/media/catalog/product/1/3/137326.jpeg?auto=webp&format=pjpg&fit=cover'),
+       (4, 'Accesorios Varios', 'https://http2.mlstatic.com/D_NQ_NP_603402-MLU71760215291_092023-O.webp'),
+	   (4, 'Cascos', 'https://libus.cl/media/catalog/product/cache/8c8d34078b36c1ef38bfd913031288cb/c/a/cascos-01_1.jpg'),
+       (4, 'Guantes', 'https://www.puntosafety.cl/wp-content/uploads/2020/08/1047-Guante-Multiflex.jpg'),
+       (6, 'Huincha', 'https://ferreteriaprat.cl/media/catalog/product/h/u/hui028943_10_hclrbe2e3gvocv3u.jpg?optimize=medium&bg-color=255,255,255&fit=bounds&height=700&width=700&canvas=700:700'),
+       (6, 'Medidores',  'https://inalambricoschile.vtexassets.com/arquivos/ids/158629-800-auto?v=637719314713200000&width=800&height=auto&aspect=true');
+
+
 -- Productos
-insert into productos(codigo, subcat, marca, nombre, precio)
+insert into productos(codigo, subcat_id, marca, nombre, precio)
 values 
 (1, 1, 'Fischer', 'Tornillo para Madera', 300),
 (2, 5, 'Stanley', 'Martillo de Carpintero', 5500),
@@ -32,8 +74,10 @@ values
 (29, 8, 'Bosch', 'Nivel Láser', 9700),
 (30, 6, 'Bosch', 'Medidor de Distancia', 8800);
 
+
+
 -- Stock
-insert into stock(prod_id, sucursal_id, stock)
+insert into stocks(prod_id, sucursal_id, stock)
 values 
 (1, 1, 45), (1, 2, 12), (1, 3, 1), (1, 4, 0),
 (2, 1, 30), (2, 2, 20), (2, 3, 10), (2, 4, 5),
@@ -67,7 +111,7 @@ values
 (30, 1, 30), (30, 2, 25), (30, 3, 20), (30, 4, 15);
 
 -- Productos
-insert into productos(codigo, subcat, marca, nombre, precio)
+insert into productos(codigo, subcat_id, marca, nombre, precio)
 values 
 (31, 1, 'Hilti', 'Tornillo Autotaladrante', 400),
 (32, 2, 'Stanley', 'Martillo de Bola', 5700),
@@ -101,7 +145,7 @@ values
 (60, 7, 'Bosch', 'Medidor de Espesor', 9200);
 
 -- Stock
-insert into stock(prod_id, sucursal_id, stock)
+insert into stocks(prod_id, sucursal_id, stock)
 values 
 (31, 1, 50), (31, 2, 40), (31, 3, 30), (31, 4, 20),
 (32, 1, 35), (32, 2, 25), (32, 3, 15), (32, 4, 5),
@@ -136,7 +180,7 @@ values
 
 
 -- Promociones
-insert into promociones(producto, preciop, iniciop, finalp)
+insert into promociones(producto_id, preciop, iniciop, finalp)
 values 
 (1, 2200, '2024-06-01', '2024-06-30'),
 (3, 8700, '2024-06-05', '2024-06-25'),
@@ -160,3 +204,13 @@ values
 (39, 15800, '2024-06-15', '2024-06-30');
 
 
+insert into tipo_usuarios(tipo_id, nombre_tipo)
+values (0, 'Administrador'),
+	   (1, 'Cliente');
+
+-- Usuarios
+
+insert into usuarios(nombre, apellido, pass, correo, tipo_user)
+values ("Bruno", "Bevilaqcua", "perroChocolo123", "brunitoAlbo666@gmail.com", 1),
+       ("Daniel", "Gutierrez", "CojoJulio123", "cojitojulio@gmail.com", 0),
+       ("Michele", "Mouton", "MichelleQuattro51", "michelequattro@gmail.com", 1);
